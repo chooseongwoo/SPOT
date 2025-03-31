d020725e7532b2ff1c1775ac2e939cead93328bb
추성우 <cntjddn715@gmail.com>
2025-03-31 12:28:56 +0900

    chore :: init theme

---
+++ styles/theme.ts
@@ -0,0 +1,20 @@
+const theme = {
+  green: {
+    1: "#EAFAF4",
+    2: "#2AD18E",
+    3: "#26BC80",
+    4: "#22A772",
+  },
+  gray: {
+    1: "#F9F9F9",
+    2: "#E5E5E5",
+    3: "#C3C3C3",
+    4: "#8C8C8C",
+    5: "#5E5E5E",
+  },
+  white: "#FFF",
+  black: "#222",
+  red: "#F36563",
+};
+
+export default theme;
