b793790c70a9ab58f658b7409450c949ce05f48e
추성우 <cntjddn715@gmail.com>
2025-03-31 19:20:54 +0900

    refactor :: change units to fit RN

--- styles/font.ts
+++ styles/font.ts
@@ -1,30 +1,30 @@
-const fontGenerator = (weight: number, size: number, lineHeight: number) => ({
-  "font-weight": weight,
-  "font-size": `${size}rem`,
-  "line-height": `${lineHeight}%`,
+const fontGenerator = (weight: number, size: number, lhRatio: number) => ({
+  "font-weight": weight.toString(),
+  "font-size": size,
+  "line-height": parseFloat((size * (lhRatio / 100)).toFixed(2)),
 });
 
 const font = {
-  large: fontGenerator(700, 2.125, 140),
+  large: fontGenerator(700, 34, 140),
 
-  T1: fontGenerator(700, 1.75, 140),
-  T2: fontGenerator(700, 1.5, 140),
-  T3: fontGenerator(700, 1.25, 140),
+  T1: fontGenerator(700, 28, 140),
+  T2: fontGenerator(700, 24, 140),
+  T3: fontGenerator(700, 20, 140),
 
-  headline: fontGenerator(600, 1.125, 140),
+  headline: fontGenerator(600, 18, 140),
 
-  B1: fontGenerator(500, 1.125, 150),
-  B2: fontGenerator(500, 1, 150),
-  B3: fontGenerator(500, 0.875, 150),
+  B1: fontGenerator(500, 18, 150),
+  B2: fontGenerator(500, 16, 150),
+  B3: fontGenerator(500, 14, 150),
 
-  footnote: fontGenerator(400, 0.8125, 150),
+  footnote: fontGenerator(400, 13, 150),
 
-  cap1: fontGenerator(500, 0.75, 120),
-  cap2: fontGenerator(500, 0.6875, 120),
+  cap1: fontGenerator(500, 12, 120),
+  cap2: fontGenerator(500, 11, 120),
 
-  btn1: fontGenerator(600, 1.125, 100),
-  btn2: fontGenerator(600, 1, 100),
-  btn3: fontGenerator(500, 0.875, 100),
+  btn1: fontGenerator(600, 18, 100),
+  btn2: fontGenerator(600, 16, 100),
+  btn3: fontGenerator(500, 14, 100),
 };
 
 export default font;
