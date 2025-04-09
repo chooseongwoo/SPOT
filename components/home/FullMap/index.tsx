3fb123c8274352c9b78301673979ff9bc5aa27e4
추성우 <cntjddn715@gmail.com>
2025-04-09 16:40:51 +0900

    feat :: float the user location on the map.

--- components/home/FullMap/index.tsx
+++ components/home/FullMap/index.tsx
@@ -1,21 +1,76 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Platform } from "react-native";
 import * as Device from "expo-device";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import MapView, { PROVIDER_GOOGLE, Region } from "react-native-maps";
 import { customMapStyle } from "@/assets/customMapStyle";
+import Geolocation, {
+  GeoPosition,
+  GeoError,
+} from "react-native-geolocation-service";
+
+const requestPermission = async () => {
+  try {
+    if (Platform.OS === "ios") {
+      return await Geolocation.requestAuthorization("always");
+    }
+    return "granted";
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+};
 
 const FullMap = () => {
-  return (
-    <MapView
-      provider={
-        Platform.OS === "android" || (Platform.OS === "ios" && Device.isDevice)
-          ? PROVIDER_GOOGLE
-          : undefined
+  const [location, setLocation] = useState<GeoPosition | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const result = await requestPermission();
+      if (result === "granted") {
+        Geolocation.getCurrentPosition(
+          (position: GeoPosition) => {
+            setLocation(position);
+          },
+          (error: GeoError) => {
+            console.log("위치 에러:", error);
+          },
+          {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 10000,
+          }
+        );
       }
-      style={{ flex: 1 }}
-      mapType="terrain"
-      customMapStyle={customMapStyle}
-    />
+    })();
+  }, []);
+
+  const region: Region | undefined = location
+    ? {
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.002,
+        longitudeDelta: 0.002,
+      }
+    : undefined;
+
+  return (
+    <>
+      {region && (
+        <MapView
+          provider={
+            Platform.OS === "android" ||
+            (Platform.OS === "ios" && Device.isDevice)
+              ? PROVIDER_GOOGLE
+              : undefined
+          }
+          style={{ flex: 1 }}
+          initialRegion={region}
+          showsUserLocation={true}
+          mapType="terrain"
+          customMapStyle={customMapStyle}
+        />
+      )}
+    </>
   );
 };
 
---
+++ ios/map.swift
@@ -0,0 +1,7 @@
+//
+//  map.swift
+//  SPOT
+//
+//  Created by 추성우 on 4/9/25.
+//
+
