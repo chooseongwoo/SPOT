1a9145366d6726b4364775a2a01f66491ae4a540
추성우 <cntjddn715@gmail.com>
2025-04-10 00:33:48 +0900

    chore :: declare geocode type

---
+++ types/googleGeocode.type.ts
@@ -0,0 +1,23 @@
+export interface GoogleGeocodeResult {
+  formatted_address: string;
+  address_components: AddressComponent[];
+  geometry: {
+    location: {
+      lat: number;
+      lng: number;
+    };
+    location_type: string;
+    viewport: {
+      northeast: { lat: number; lng: number };
+      southwest: { lat: number; lng: number };
+    };
+  };
+  place_id: string;
+  types: string[];
+}
+
+interface AddressComponent {
+  long_name: string;
+  short_name: string;
+  types: string[];
+}
