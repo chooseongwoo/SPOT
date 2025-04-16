b980d509cbc467a9c3c8a6a59bff18f380378edd
추성우 <cntjddn715@gmail.com>
2025-04-17 08:48:52 +0900

    chore :: 상대경로 import

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,16 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['nativewind/babel', 'react-native-reanimated/plugin'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+    'nativewind/babel',
+    'react-native-reanimated/plugin',
+  ],
 };
